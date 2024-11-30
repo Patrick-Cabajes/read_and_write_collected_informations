60a909a7dbd9c5faf6ee82068e23ee8b0f4f8519
Patrick <patrickcabajes07@gmail.com>
2024-11-30 20:31:16 +0800

    I modified the messages for this program

--- read_collected_information.py
+++ read_collected_information.py
@@ -2,13 +2,10 @@ while True:
     name_search = input("Please input the full name of the person you want to search for: ")
 
     if not name_search.replace(" ", "").isalpha():
-        print("Invalid Input! Please input letters only.")
+        print("Invalid Input! The information stored for name contains letters only.")
 
     elif not name_search.istitle():
-        print("Invalid Input! The first letter in each word should be capitalized, followed by lowercase characters.")
-
-    elif len(name_search) <3:
-        print("Invalid Input! Please provide a longer name.")
+        print("Invalid Input! The first letter in each word of the stored information for name is capitalized, followed by lowercase characters.")
 
     else:
         break
