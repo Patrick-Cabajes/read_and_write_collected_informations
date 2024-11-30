5943801dc4dfb6d620804fc45d6f5e8c34ef2357
Patrick <patrickcabajes07@gmail.com>
2024-11-30 21:37:50 +0800

    I'm trying to fix the problem, the program should only show the information of the person being asked

--- read_collected_information.py
+++ read_collected_information.py
@@ -1,14 +1,24 @@
 while True:
-    name_search = input("Please input the full name of the person you want to search for: ")
+    while True:
+        name_search = input("Please input the full name of the person you want to search for: ")
 
-    if not name_search.replace(" ", "").isalpha():
-        print("Invalid Input! The information stored for name contains letters only.")
+        if not name_search.replace(" ", "").isalpha():
+            print("Invalid Input! The information stored for name contains letters only.")
 
-    elif not name_search.istitle():
-        print("Invalid Input! The first letter in each word of the stored information for name is capitalized, followed by lowercase characters.")
+        elif not name_search.istitle():
+            print("Invalid Input! The first letter in each word of the stored information for name is capitalized, followed by lowercase characters.")
 
-    else:
-        break
+        else:
+            break
+
+    with open("collected_informations.txt", "r") as file:
+        lines = file.readlines()
+
+        for line in lines:
+            print(line.strip())
+        
+
+            
 #open the text file created from the previous program and read its contents
 
     #using for loop, display informations about the person the user searched for
