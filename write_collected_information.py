f11c77c3aa57f7c1cad5c07e0e76220662bf51f6
Patrick <patrickcabajes07@gmail.com>
2024-11-30 14:22:46 +0800

    I have finally fixed the problem and can now proceed to add more information to be collected

--- write_collected_information.py
+++ write_collected_information.py
@@ -1,23 +1,22 @@
 informations = {}
-with open("./collected_informations.txt", "a") as info:
 
+while True:
     while True:
-        while True:
-            full_name = input("Please input your full name: ")
-            if not full_name.replace(" ", "").isalpha():
-                print("Invalid Input: Please input letters only!")
+        full_name = input("Please input your full name: ")
+        if not full_name.replace(" ", "").isalpha():
+            print("Invalid Input: Please input letters only!")
 
-            elif not full_name.istitle():
-                print("Invalid Input: The first letter in each word should be capitalized, followed by lowercase characters!")
+        elif not full_name.istitle():
+            print("Invalid Input: The first letter in each word should be capitalized, followed by lowercase characters!")
 
-            elif len(full_name) <3:
-                print("Invalid Input: Please provide a longer name!")
+        elif len(full_name) <3:
+            print("Invalid Input: Please provide a longer name!")
 
-            else:
-                break
+        else:
+            break
 
     #ask for more information following the format from the second loop
-
+    with open("./collected_informations.txt", "a") as info:
         informations = {
             info.write(f'Name: {full_name}\n'),
         }
