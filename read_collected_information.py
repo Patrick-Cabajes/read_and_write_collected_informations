ce668563696375d258d446bd20d57f207dd6b47b
Patrick <patrickcabajes07@gmail.com>
2024-11-30 20:29:08 +0800

    For name search, I added the same conditions from the previous file for easy browsing

--- read_collected_information.py
+++ read_collected_information.py
@@ -1,4 +1,17 @@
-#ask user for the full name they want to search for
+while True:
+    name_search = input("Please input the full name of the person you want to search for: ")
+
+    if not name_search.replace(" ", "").isalpha():
+        print("Invalid Input! Please input letters only.")
+
+    elif not name_search.istitle():
+        print("Invalid Input! The first letter in each word should be capitalized, followed by lowercase characters.")
+
+    elif len(name_search) <3:
+        print("Invalid Input! Please provide a longer name.")
+
+    else:
+        break
 #open the text file created from the previous program and read its contents
 
     #using for loop, display informations about the person the user searched for
