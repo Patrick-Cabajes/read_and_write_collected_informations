bcfa6a44bcca991f69e0d6e369837ef90acbe238
Patrick <patrickcabajes07@gmail.com>
2024-11-30 12:30:09 +0800

    My pseudocode

---
+++ write_collected_information.py
@@ -0,0 +1,15 @@
+#declare an empty array
+#open a text file
+
+#while True:
+    #while True:
+        #ask the user to input their name
+        #add conditions for the input
+            #print error messages if the input is invalid
+        
+    #ask for more information following the format from the second loop
+
+    #store the information in the array and write the collected data to the text file
+
+    #ask the user if they would like to input another entry
+        #If the response is "no," print a message indicating that the data has been written to the file.
