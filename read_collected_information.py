a6d4c16987cac97748c5e200a72c1ccd9717962c
Patrick <patrickcabajes07@gmail.com>
2024-11-30 19:36:50 +0800

    my pseudocode for the 2nd program

---
+++ read_collected_information.py
@@ -0,0 +1,6 @@
+#ask user for the full name they want to search for
+#open the text file created from the previous program and read its contents
+
+    #using for loop, display informations about the person the user searched for
+    #if the name was not found:    
+        #print that the person the user was looking for was not found
