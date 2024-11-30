66c510f19bed375833db510ffaa1adf8969fc061
Patrick <patrickcabajes07@gmail.com>
2024-11-30 14:04:10 +0800

    The input name doesn't get written to the text file

--- write_collected_information.py
+++ write_collected_information.py
@@ -1,18 +1,26 @@
 informations = {}
-#open a text file
+with open("./collected_informations.txt", "a") as info:
 
-while True:
     while True:
-        full_name = input("Please input your full name: ")
-        if not full_name.isalpha():
-            print("Invalid Input: Please input letter only")
+        while True:
+            full_name = input("Please input your full name: ")
+            if not full_name.replace(" ", "").isalpha():
+                print("Invalid Input: Please input letters only!")
 
-        else:
-            break
+            elif not full_name.istitle():
+                print("Invalid Input: The first letter in each word should be capitalized, followed by lowercase characters!")
+
+            elif len(full_name) <3:
+                print("Invalid Input: Please provide a longer name!")
+
+            else:
+                break
 
     #ask for more information following the format from the second loop
 
-    #store the information in the array and write the collected data to the text file
+        informations = {
+            info.write(f'Name: {full_name}\n'),
+        }
 
     #ask the user if they would like to input another entry
         #If the response is "no," print a message indicating that the data has been written to the file.
